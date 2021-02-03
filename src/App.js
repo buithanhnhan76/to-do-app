820df31931f3ae2b50abe32bcd9e0ce3408bbd9d
Nhan <nhanbui37@gmail.com>
2021-02-03 17:21:34 +0700

    continue in a minute ...

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import Clock from './Components/Clock';
+
+function App  (){
+  return (
+    <div>
+        <Clock />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Clock.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class Clock extends Component{
+    constructor(props){
+      super(props);
+      this.state = {date: new Date()};
+    };
+  
+    tick(){
+      this.setState({date: new Date()});
+    };
+  
+    render(){
+      return (
+        <div>
+          <h1>Hello, World!</h1>
+          <h2>It is {this.state.date.toLocaleDateString()}. </h2>
+        </div>
+      )
+    };
+  };
+
+export default Clock;
